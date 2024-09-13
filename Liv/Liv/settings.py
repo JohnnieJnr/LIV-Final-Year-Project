4b6f424dd7451e8c1086ba4e35f77d74ceaa7b8c
Johnnie <52750509+JohnnieJnr@users.noreply.github.com>
2024-09-13 13:28:57 +0000

    Update settings.py

--- Liv/Liv/settings.py
+++ Liv/Liv/settings.py
@@ -33,6 +33,29 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_METHODS = [
+   'DELETE',
+   'GET',
+   'OPTIONS',
+   'PATCH',
+   'POST',
+   'PUT',
+ ]
+
+CORS_ALLOW_HEADERS = [
+   'accept',
+   'accept-encoding',
+   'authorization',
+   'content-type',
+   'dnt',
+   'origin',
+   'user-agent',
+   'x-csrftoken',
+   'x-requested-with',
+ ]
+
 # Application definition
 
 INSTALLED_APPS = [
