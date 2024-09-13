58cfa8c4b2f4b8660b748b2261375acd70bc7690
FiifiQwontwo <quophi555@gmail.com>
2024-09-13 17:19:22 +0000

    as

--- Liv/accounts/models.py
+++ Liv/accounts/models.py
@@ -10,7 +10,7 @@ class Account(AbstractBaseUser):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50, unique=True)
     phone = models.CharField(max_length=50, unique=True)
-    username = models.CharField(max_length=100, unique=True)
+    username = models.CharField(max_length=100, Blank=True, null =True)
     # required Fields
     date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
     last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
