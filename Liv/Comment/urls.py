5c217ab45f1f57a7fa1f50527359dbf39e64c0f4
Johnnie <52750509+JohnnieJnr@users.noreply.github.com>
2024-09-17 13:53:29 +0000

    Update urls.py

--- Liv/Comment/urls.py
+++ Liv/Comment/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ListCommentView
+from .views import ListCommentView, CreateComment
 
 app_name = 'comment'
 
 urlpatterns = [
     path('cs/', ListCommentView.as_view(), name='listcomment_endpoint'),
+    path('create/', CreateComment.as_view(), name='new_comment_endpoint'),
 
 ]
